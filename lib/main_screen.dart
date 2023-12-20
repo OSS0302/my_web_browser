681403320354683c11e8a04c0f934fb2602e6f77
Ohsangsoo <tkdtn3264@gmail.com>
2023-12-20 22:10:53 +0900

    080. WebView 기본 설정

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({super.key});
@@ -7,13 +10,23 @@ class MainScreen extends StatefulWidget {
   State<MainScreen> createState() => _MainScreenState();
 }
 
+
 class _MainScreenState extends State<MainScreen> {
+  @override
+  void initState() {
+    super.initState();
+    if(Platform.isAndroid)WebView.platform = AndroidWebView();
+  }
+
   @override
   Widget build(BuildContext context) {
     return  Scaffold(
       appBar: AppBar(
         title: Text('나만의 웹 브라우저'),
       ),
+      body: WebView(
+      initialUrl : 'https://flutter.dev',
+    ),
     );
   }
 }
