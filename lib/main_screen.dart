b78b503fd326b83ccebc110d32578daabbd7dff2
Ohsangsoo <tkdtn3264@gmail.com>
2023-12-20 22:19:28 +0900

    081. 옵션 메뉴

--- lib/main_screen.dart
+++ lib/main_screen.dart
@@ -10,23 +10,47 @@ class MainScreen extends StatefulWidget {
   State<MainScreen> createState() => _MainScreenState();
 }
 
-
 class _MainScreenState extends State<MainScreen> {
   @override
   void initState() {
     super.initState();
-    if(Platform.isAndroid)WebView.platform = AndroidWebView();
+    if (Platform.isAndroid) WebView.platform = AndroidWebView();
   }
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
-        title: Text('나만의 웹 브라우저'),
+        title: const Text('나만의 웹 브라우저'),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: const Icon(Icons.add),
+          ),
+          PopupMenuButton<String>(
+            onSelected:(value){
+              print(value);
+            },
+            itemBuilder: (context) => [
+              const PopupMenuItem<String>(
+                value: 'https://www.google.com/',
+                child: Text('구글'),
+              ),
+              const PopupMenuItem<String>(
+                value: 'https://www.naver.com/',
+                child: Text('네이버'),
+              ),
+              const PopupMenuItem<String>(
+                value: 'https://www.kakao.com/',
+                child: Text('카카오'),
+              ),
+            ],
+          )
+        ],
       ),
-      body: WebView(
-      initialUrl : 'https://flutter.dev',
-    ),
+      body: const WebView(
+        initialUrl: 'https://flutter.dev',
+      ),
     );
   }
 }
